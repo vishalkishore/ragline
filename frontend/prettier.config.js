8e98912b628dde880a0172e9422dcf3bb0cea908
Vishal <kishorevishal01@gmail.com>
2025-05-09 07:09:51 +0530

    setup frontend configuration

---
+++ frontend/prettier.config.js
@@ -0,0 +1,10 @@
+export default {
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 80,
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  bracketSameLine: false,
+}
